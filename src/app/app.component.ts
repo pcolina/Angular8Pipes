510db721d559144e4a1fbed0d85c6a064f1b0361
pcolina <pabloangelcolina@gmail.com>
2020-01-27 11:47:21 +0100

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+ 
+
 export class AppComponent {
   title = 'pipes';
+
+  nombre = 'Pablo Angel';
+array = [1, 2, 3,4,5,6,7,8,9,10];
+
+
+a = 0.234; 
+b = 12345;
 }
